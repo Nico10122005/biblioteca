9e0cdf38ee326ecbe6bd31b658cb0e8706de6499
Nico10122005 <nico10122005@gmail.com>
2024-04-22 10:46:54 +0200

    add

--- database.js
+++ database.js
@@ -3,9 +3,9 @@
     console.log();
     const connpool = mysql.createPool({
         host: "127.0.0.1",
-        user: "root",
-        password: "root",
-        database: "td"
+        user: "TPS",
+        password: "tttpppsss",
+        database: "biblioteca"
     }, { debug: true });
     
     connpool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {

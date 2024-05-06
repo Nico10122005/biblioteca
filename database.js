e534950c4f3299c02d3b293dc7aad10d5e3cacf7
Nico10122005 <nico10122005@gmail.com>
2024-05-06 10:24:36 +0200

    add

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
     const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
     console.log();
     const connpool = mysql.createPool({
-        host: "10.20.2.173",
+        host: "10.20.2.174",
         user: "tps",
         password: "ttppss",
         database: "biblioteca",

067b66352d91c50f6cdea4ffbb70377ef7f21e03
QuettiS <simonequetti856@gmail.com>
2024-05-24 11:33:35 +0200

    add

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get("/", (req, res, next) => {
 
 require("./endpoints/tasks.js")(app,connpool)
 require("./endpoints/libri.js")(app,connpool)
+require("./endpoints/prestiti.js")(app,connpool)
 // Insert here other API endpoints
 
 // Default response for any other request
